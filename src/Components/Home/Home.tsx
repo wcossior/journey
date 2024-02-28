23b6a62a6ba688a13028f646698f7f13d73fdba3
Wilder Cossio <w.cossio.r@gmail.com>
2024-02-28 15:06:06 -0400

    making the search form and changing the img's and videos' names

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
 import './home.css';
+import maiu from '../../assets/video1.mp4';
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
 
 const Home = () => {
   return (
     <section className='home'>
-      <div className='overlay'>
-        <video src=""></video>
+      <div className='overlay'></div>
+      <video src={maiu} muted autoPlay loop typeof='video/mp4'></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+
+          <span className="smallText">
+            Our Packages
+          </span>
+
+          <h1 className="homeTitle">
+            Search your Holiday
+          </h1>
+
+        </div>
+
+        <div className="cardDiv grid">
+
+          <div className="destinationInput">
+            <label htmlFor="city">Search your destination: </label>
+            <div className="input flex">
+              <input type="text" placeholder='Enter name here...' />
+              <GrLocation className='icon' />
+            </div>
+          </div>
+
+          <div className="dateInput">
+            <label htmlFor="date">Select your date: </label>
+            <div className="input flex">
+              <input type="date" placeholder='Enter name here...' />
+            </div>
+          </div>
+
+          <div className="priceInput">
+            <div className="labelTotal flex">
+              <label htmlFor="price">Max price: </label>
+              <h3 className='total'>$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className='icon' />
+            <span>MORE FILTERS</span>
+          </div>
+
+        </div>
+
       </div>
+
     </section>
   )
 }
